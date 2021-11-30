9d7c861e7964c3b0e62c8928aedd319b0f02b582
ChakshuGautam <chaks.gautam@gmail.com>
2021-11-30 10:32:07 +0530

    Fix: Add Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'verbose', 'log'],
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Templater')
+    .setDescription('Template Manager and Renderer')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
